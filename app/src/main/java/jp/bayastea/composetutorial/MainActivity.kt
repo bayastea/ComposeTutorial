97efe9f79261e35eea898260cdca821d9bbd8a3d
bayastea <as_takabayashi@AsuminoMacBook-Pro.local>
2023-07-04 09:04:09 +0900

    3. Material Design

--- app/src/main/java/jp/bayastea/composetutorial/MainActivity.kt
+++ app/src/main/java/jp/bayastea/composetutorial/MainActivity.kt
@@ -1,9 +1,11 @@
 package jp.bayastea.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -22,7 +24,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MessageCard(Message("Anddroid", "Jetpack Compose"))
+            ComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Anddroid", "Jetpack Compose"))
+                }
+            }
         }
     }
 }
@@ -42,24 +48,50 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 // 画像を角丸に設定
                 .clip(CircleShape)
+                .border(
+                    1.5.dp, MaterialTheme.colors.secondary, CircleShape
+                )
         )
 
         // 画像とColumnの間に横方向スペースを挿入
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             // authorとbodyの間に縦方向のスペースを挿入
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
 
+@Preview(name = "Light Mode") // 同じ関数に複数のアノテーションを追加できる
+@Preview(
+    // MaterialDesignのサポートで自動でダークテーマの設定がされる
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode" // nameはプレビューを区別するための名前
+)
+
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+    ComposeTutorialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(
+                msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
 }
